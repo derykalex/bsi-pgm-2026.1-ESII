551291735a53e60a921e9b430c783954c70a5c13
derykalex <derykalex@gmail.com>
2026-05-06 23:21:49 -0300

    Create RepositorioEmprestimo class with methods

---
+++ modelos/modelos/repositorios/repositorio_emprestimo.py
@@ -0,0 +1,22 @@
+class RepositorioEmprestimo:
+
+    def buscar_equipamento(self, equip_id: int):
+        pass
+
+    def salvar_emprestimo(self, emprestimo):
+        pass
+
+    def marcar_indisponivel(self, equip_id: int):
+        pass
+
+    def buscar_emprestimo(self, emprestimo_id: int):
+        pass
+
+    def marcar_devolvido(self, emprestimo_id: int):
+        pass
+
+    def marcar_disponivel(self, equip_id: int):
+        pass
+
+    def buscar_emprestimos_atrasados(self):
+        pass
