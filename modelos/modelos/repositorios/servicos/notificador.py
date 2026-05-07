3b3ef9866374a99783a5b591f2996b5026163c9b
derykalex <derykalex@gmail.com>
2026-05-06 23:23:44 -0300

    Add Notificador class for loan notifications

---
+++ modelos/modelos/repositorios/servicos/notificador.py
@@ -0,0 +1,10 @@
+class Notificador:
+
+    def notificar_emprestimo(self, email: str, data_devolucao):
+        pass
+
+    def notificar_devolucao(self, email: str):
+        pass
+
+    def notificar_atraso(self, email: str):
+        pass
